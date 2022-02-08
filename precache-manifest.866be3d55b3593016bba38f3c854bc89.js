700a5ae0f7735ba4ef48d9ffdcb120cf514ec2e2
Amish Menon <amishmenon@gmail.com>
2022-02-07 23:07:29 -0600

    updated gallery

---
+++ precache-manifest.866be3d55b3593016bba38f3c854bc89.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc63019b2bbdb9aac0e",
+    "url": "/menon-strength/css/app.ba9c81e7.css"
+  },
+  {
+    "revision": "e150bf0ca43b15104246",
+    "url": "/menon-strength/css/chunk-0554557a.8cfdcee7.css"
+  },
+  {
+    "revision": "69469b0728cadb202f07",
+    "url": "/menon-strength/css/chunk-4d28a4bf.c2d52dc9.css"
+  },
+  {
+    "revision": "7c3ce8ed20d31451f6f2",
+    "url": "/menon-strength/css/chunk-57182468.b799bc75.css"
+  },
+  {
+    "revision": "c8d27c575a31253f5a11",
+    "url": "/menon-strength/css/chunk-6839ba67.88a0438a.css"
+  },
+  {
+    "revision": "a1c452e0ba2838434778",
+    "url": "/menon-strength/css/chunk-vendors.ee14b75c.css"
+  },
+  {
+    "revision": "ae8e8346185c6ebed1399de077cf8542",
+    "url": "/menon-strength/img/1.ae8e8346.png"
+  },
+  {
+    "revision": "59d342c2fbdad516b378dcea71ee6fd0",
+    "url": "/menon-strength/img/12.59d342c2.png"
+  },
+  {
+    "revision": "58d57533cef90f953d5235bf24e6443b",
+    "url": "/menon-strength/img/14.58d57533.png"
+  },
+  {
+    "revision": "38d6a704d824dfb92e0d6e2a07c48649",
+    "url": "/menon-strength/img/15.38d6a704.png"
+  },
+  {
+    "revision": "4c67a9652420970b0fe2f99adc05f749",
+    "url": "/menon-strength/img/1_8.4c67a965.png"
+  },
+  {
+    "revision": "f912e70e4030bc52490614d7e2154268",
+    "url": "/menon-strength/img/2.f912e70e.png"
+  },
+  {
+    "revision": "e85a98174ab9b3420f0e2053415eec56",
+    "url": "/menon-strength/img/3.e85a9817.png"
+  },
+  {
+    "revision": "c354968252f69cd335226791acd7b358",
+    "url": "/menon-strength/img/4.c3549682.png"
+  },
+  {
+    "revision": "7643b089486236bcae42d45421732311",
+    "url": "/menon-strength/img/5.7643b089.png"
+  },
+  {
+    "revision": "859c2c823ebc178add971744a1e513f6",
+    "url": "/menon-strength/img/6.859c2c82.png"
+  },
+  {
+    "revision": "1dee2bea4739204f56903106a2558250",
+    "url": "/menon-strength/img/7.1dee2bea.png"
+  },
+  {
+    "revision": "61a45147facbb3ad3ff44b4d2e0b43b2",
+    "url": "/menon-strength/img/9.61a45147.png"
+  },
+  {
+    "revision": "0ee892f9de63876f9c169b3c24c87e87",
+    "url": "/menon-strength/img/TrainingServicesOffered.0ee892f9.png"
+  },
+  {
+    "revision": "1525a989532bbed5f04cfebe282c4af0",
+    "url": "/menon-strength/img/background-2.1525a989.jpeg"
+  },
+  {
+    "revision": "ad0544fe0b7104a4e8561613d4ebfe8c",
+    "url": "/menon-strength/img/boat-profile.ad0544fe.png"
+  },
+  {
+    "revision": "dd09cff5c42770b38e8f07947a567522",
+    "url": "/menon-strength/img/brush-stroke-banner-7.dd09cff5.png"
+  },
+  {
+    "revision": "0f4ca023a20ee37ce5ad8f77463ba421",
+    "url": "/menon-strength/img/menon-strength-profile.0f4ca023.jpeg"
+  },
+  {
+    "revision": "bde0a2f58dc45b33bdee73373770aa89",
+    "url": "/menon-strength/index.html"
+  },
+  {
+    "revision": "fdc63019b2bbdb9aac0e",
+    "url": "/menon-strength/js/app.fb37ad54.js"
+  },
+  {
+    "revision": "e150bf0ca43b15104246",
+    "url": "/menon-strength/js/chunk-0554557a.ccbc7dac.js"
+  },
+  {
+    "revision": "69469b0728cadb202f07",
+    "url": "/menon-strength/js/chunk-4d28a4bf.11baf530.js"
+  },
+  {
+    "revision": "7c3ce8ed20d31451f6f2",
+    "url": "/menon-strength/js/chunk-57182468.3de5805f.js"
+  },
+  {
+    "revision": "c8d27c575a31253f5a11",
+    "url": "/menon-strength/js/chunk-6839ba67.26c9135d.js"
+  },
+  {
+    "revision": "a1c452e0ba2838434778",
+    "url": "/menon-strength/js/chunk-vendors.9fe577b9.js"
+  },
+  {
+    "revision": "fc0df7af46305ba3de66711e4096182a",
+    "url": "/menon-strength/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/menon-strength/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/menon-strength/precache-manifest.342b13688d754590f45678c56018a4c9.js"
+  "/menon-strength/precache-manifest.866be3d55b3593016bba38f3c854bc89.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "menon-strength"});
